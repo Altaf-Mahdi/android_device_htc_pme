a46fcf7f34ca7a2605038590606ed014bc45f83a
Altaf-Mahdi <altaf.mahdi@gmail.com>
2017-04-05 06:22:03 +0100

    Revert "pme: switch to schedutil governor"
    This reverts commit a5f0521d62564161672d944dbb5af55bb8784e52.
    
    Change-Id: I7e0e5a1151331f3e9938ce58de798a703d1626da

--- rootdir/bin/init.power.sh
+++ rootdir/bin/init.power.sh
@@ -27,8 +27,8 @@ write /sys/module/lowmemorykiller/parameters/minfree "27648,41472,48384,72378,84
 write /sys/module/lowmemorykiller/parameters/vmpressure_file_min 159375
 
 # if EAS is present, switch to sched governor (no effect if not EAS)
-write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor "schedutil"
-write /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor "schedutil"
+write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor "sched"
+write /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor "sched"
 
 # re-enable thermal hotplug
 write /sys/module/msm_thermal/core_control/enabled 1
