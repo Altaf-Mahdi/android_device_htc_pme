aa0aaee42337b16b9109eddacb6ffad502ad1fc8
altaf-mahdi <altaf.mahdi@gmail.com>
2017-09-11 12:22:48 +0100

    [TEMP] pme: disable lineage overlays

--- device.mk
+++ device.mk
@@ -22,8 +22,8 @@ $(call inherit-product-if-exists, vendor/htc/pme/pme-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
+#    $(LOCAL_PATH)/overlay-lineage
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
