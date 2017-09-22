3b5e6a1f6a77322ce1199f565e0e34a0416420ed
altaf-mahdi <altaf.mahdi@gmail.com>
2017-09-22 10:47:23 +0100

    pme: build camera hal

--- device.mk
+++ device.mk
@@ -119,10 +119,9 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    libshim_camera
-
-# Snap
-PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@3.2-impl \
+    libshim_camera \
     Snap
 
 # Connectivity Engine support (CNE)
